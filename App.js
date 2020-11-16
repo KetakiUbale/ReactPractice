9d602b495b8e2a79da6d2b436ef60bbbf1ed9218
KetakiUbale <70467081+KetakiUbale@users.noreply.github.com>
2020-11-16 16:39:09 +0530

    React practice

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Vehicle from './Vehicle/vehicle';
+
+function App() {
+    return ( < div className = "App" >
+
+        <
+        Vehicle vehicleType = "Car"
+        name = "Altroz"
+        manufacturer = "Tata"
+        fuel = "Petrol" / >
+        <
+        Vehicle vehicleType = "Car"
+        name = "Seltos"
+        manufacturer = "Kia"
+        fuel = "Diesel" >
+        This is a brand new car by Kia Motors < /Vehicle>    <
+        /div>
+    );
+}
+
+export default App;
