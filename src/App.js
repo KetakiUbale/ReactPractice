c3f9886cb298897b25396df4ace696a5e61a5b4c
ubaleketaki@gmail.com <ubaleketaki@gmail.com>
2020-11-16 16:03:54 +0530

    First Functional Component Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Vehicle from './Vehicle/vehicle';
 
 function App() {
-  return (
-    <div className="App">
-   
-   </div>
-  );
+    return ( <
+        div className = "App" >
+
+        <
+        Vehicle vehicleType = "Car"
+        name = "Altroz"
+        manufacturer = "Tata"
+        fuel = "Petrol" / >
+        <
+        Vehicle vehicleType = "Car"
+        name = "Seltos"
+        manufacturer = "Kia"
+        fuel = "Diesel" >
+        This is a brand new car by Kia Motors <
+        /Vehicle>  <
+        /div>
+    );
 }
 
 export default App;
