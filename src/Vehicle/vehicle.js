c1ecb5a9b4cebc9a3c377f67cd081920fe423af8
ubaleketaki@gmail.com <ubaleketaki@gmail.com>
2020-11-17 17:32:16 +0530

    initial commit

--- src/Vehicle/vehicle.js
+++ src/Vehicle/vehicle.js
@@ -1,14 +1,15 @@
 import './vehicle.css'
 const vehicle = props => ( <
     div className = "car" >
+
     <
     h1 >
     Vehicle: { props.vehicleType } <
     /h1> <
-    p > Name: { props.name } < /p> <
-    p > Manufaturere: { props.manufacturer } < /p> <
-    p > Fuel Type: { props.fuel } < /p> <
-    p > Comment: { props.children } < /p> <
+    p > Name: { props.name } < /p>   <
+    p > Manufaturere: { props.manufacturer } < /p>   <
+    p > Fuel Type: { props.fuel } < /p>   <
+    p > Comment: { props.children } < /p>   <
     /div>
 );
 export default vehicle;
