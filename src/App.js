1455c65f7a716db72e9abdae1f40afff93a86ea6
ubaleketaki@gmail.com <ubaleketaki@gmail.com>
2020-11-24 16:57:14 +0530

    Show and hide button added with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,96 +1,123 @@
-import React from 'react';
+import {useState} from 'react';
 import './App.css';
 import Vehicle from './Vehicle/vehicle';
 
-class App extends React.Component {
-   constructor(props){
-       super(props);
+const App = props => {
+  const [vehicleState,setVehicleState] = useState(
+  {
+  vehicles: [
+       { VehicleType: "Car", Name:"Altroz", Manufacturer: "Tata", FuelType: "Petrol" },
+       { VehicleType: "Car", Name: "Nexon", Manufacturer: "Tata", FuelType: "Electric" },
+       { VehicleType: "Car", Name: "XUV", Manufacturer: "Mahindra", FuelType: "Diesel" },
+       { VehicleType: "Car", Name: "Baleno", Manufacturer: "Maruti Suzuki", FuelType: "Petrol" },
+       ],
+        Count :4,
+        showVehicle: true,
+        showVehicleLable:"Show Vehicle"
+ });
 
-       this.state = {
-           vehicles: [
-               { VehicleType: "Car", Name: "Altroz", Manufacturer: "Tata", FuelType: "Petrol" },
-               { VehicleType: "Car", Name: "Nexon", Manufacturer: "Tata", FuelType: "Electric" },
-               { VehicleType: "Car", Name: "XUV", Manufacturer: "Mahindra", FuelType: "Diesel" },
-               { VehicleType: "Car", Name: "Baleno", Manufacturer: "Maruti Suzuki", FuelType: "Petrol" },
-           ],
-           Count:4
-       };
-   }
 
-     render() {
-    const updateHandler= (VehicleType) => {
-        this.setState ( {
+const updateHandler= (VehicleType) => {
+        setVehicleState ( {
             vehicles: [
-                { VehicleType: VehicleType, Name: "Jaguar XF", Manufacturer: "Jaguar", FuelType: "Petrol" },
-                { VehicleType: VehicleType, Name: "CONTINENTAL GT MULLINER", Manufacturer: "Bentley", FuelType: "Electric" },
-                { VehicleType: VehicleType, Name: "911 Carrera S", Manufacturer: "Porche", FuelType: "Petrol" },
-                { VehicleType: VehicleType, Name: "DBS Superleggera", Manufacturer: "Aston Martin", FuelType: "Petrol" },
+                { VehicleType: "Car", Name: "Jaguar XF", Manufacturer: "Jaguar", FuelType: "Petrol" },
+                { VehicleType: "Car", Name: "CONTINENTAL GT MULLINER", Manufacturer: "Bentley", FuelType: "Electric" },
+                { VehicleType: "Car", Name: "911 Carrera S", Manufacturer: "Porche", FuelType: "Petrol" },
+                { VehicleType: "Car", Name: "DBS Superleggera", Manufacturer: "Aston Martin", FuelType: "Petrol" },
             ],
-            Count:4
+            Count:vehicleState.Count,
+            showVehicles :vehicleState.showVehicles,
+            showVehicleLable:vehicleState.showVehicleLable
         });
-
-    }
+}
 
 const nameChangeHandler=(event)=>{
-    this.setState({
+    setVehicleState({
         vehicles: [
             { VehicleType: "Car", Name: event.target.value, Manufacturer: "Tata", FuelType: "Petrol" },
                { VehicleType: "Car", Name: "Nexon", Manufacturer: "Tata", FuelType: "Electric" },
                { VehicleType: "Car", Name: "XUV", Manufacturer: "Mahindra", FuelType: "Diesel" },
                { VehicleType: "Car", Name: "Baleno", Manufacturer: "Maruti Suzuki", FuelType: "Petrol" },
         ],
+        Count :vehicleState.Count,
+        showVehicles:vehicleState.showVehicles,
+        showVehicleLable:vehicleState.showVehicleLable
+    }
+  )};
+  
 
+   const toggleVehicleListHandler= () =>{
+     let toggleVehicle=!vehicleState.showVehicles;
+      let label= "";
+     
+     if(toggleVehicle){
+       label="Hide Vehicles";
+      }
+     else{
+         label="Show Vehicles ";
+       }
+      
+     setVehicleState({
+     vehicles: vehicleState.vehicles,
+     Count: vehicleState.Count,
+     showVehicles: toggleVehicle,
+     showVehicleLable: label
     });
-}
+  }
+  let vehicleList=null;
+  if(vehicleState.showVehicles){
+    vehicleList=<div className="row"> 
+    <Vehicle 
+    VehicleType={vehicleState.vehicles[0].VehicleType} 
+    Name={vehicleState.vehicles[0].Name} 
+    Manufacturer={vehicleState.vehicles[0].Manufacturer} 
+    FuelType={vehicleState.vehicles[0].FuelType} 
+    updateClick={updateHandler}
+    nameChanged={nameChangeHandler}>
+
+    </Vehicle>
+    
+
+     <Vehicle 
+     VehicleType={vehicleState.vehicles[1].VehicleType} 
+     Name={vehicleState.vehicles[1].Name} 
+     Manufacturer={vehicleState.vehicles[1].Manufacturer} 
+     FuelType={vehicleState.vehicles[1].FuelType}>
+
+     </Vehicle>
+
+     <Vehicle VehicleType={vehicleState.vehicles[2].VehicleType}
+     Name={vehicleState.vehicles[2].Name} 
+     Manufacturer={vehicleState.vehicles[2].Manufacturer} 
+     FuelType={vehicleState.vehicles[2].FuelType}>
+
+     </Vehicle>
+
+     <Vehicle VehicleType={vehicleState.vehicles[3].VehicleType} 
+     Name={vehicleState.vehicles[3].Name} 
+     Manufacturer={vehicleState.vehicles[3].Manufacturer} 
+     FuelType={vehicleState.vehicles[3].FuelType}>
+         
+     </Vehicle>
+     </div>
+
+  }
 
 
    return (
         <div className="App container-fluid">
-        <div className="row">
-        <div className="col-lg-6 ">
-            <h1>List Of Vehicles | Total Vehicles:{this.state.Count} </h1>
-            </div>  
-            <div className="col-lg-6 buttonDiv">
-            <button className="btn btn-primary btn-lg buttonUpdate" onClick={updateHandler.bind(this,"Sports Car")}>Update List</button>
-            </div>
-            </div>
-            <div className="row"> 
-           <Vehicle 
-           VehicleType={this.state.vehicles[0].VehicleType} 
-           Name={this.state.vehicles[0].Name} 
-           Manufacturer={this.state.vehicles[0].Manufacturer} 
-           FuelType={this.state.vehicles[0].FuelType} 
-           updateClick={updateHandler.bind(this,"Sports Car")}
-           nameChanged={nameChangeHandler}></Vehicle>
-           
-
-            <Vehicle 
-            VehicleType={this.state.vehicles[1].VehicleType} 
-            Name={this.state.vehicles[1].Name} 
-            Manufacturer={this.state.vehicles[1].Manufacturer} 
-            FuelType={this.state.vehicles[1].FuelType}>
-
-            </Vehicle>
-
-            <Vehicle VehicleType={this.state.vehicles[2].VehicleType}
-            Name={this.state.vehicles[2].Name} 
-            Manufacturer={this.state.vehicles[2].Manufacturer} 
-            FuelType={this.state.vehicles[2].FuelType}>
-
-            </Vehicle>
-
-            <Vehicle VehicleType={this.state.vehicles[3].VehicleType} 
-            Name={this.state.vehicles[3].Name} 
-            Manufacturer={this.state.vehicles[3].Manufacturer} 
-            FuelType={this.state.vehicles[3].FuelType}>
-                
-            </Vehicle>
-        </div>
-        </div>
- 
-        
-    );
-   };
+          <div className="row">
+              <div className="col-lg-6 ">
+                 <h1>List Of Vehicles | Total Vehicles:{vehicleState.Count} </h1>
+               </div>  
+                  <div className="col-lg-6 buttonDiv">
+                    <button className="btn btn-primary btn-lg buttonUpdate" onClick={updateHandler}>Update List</button>
+                    <button className="btn btn-secondary btn-lg buttonUpdate" onClick={toggleVehicleListHandler}>{vehicleState.showVehicleLable}</button>
+                 </div>
+              </div>
+              {vehicleList}
+              </div>
+   );
 }
 
 export default App;
